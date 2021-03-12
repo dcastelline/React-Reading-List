00f6e3a111d41bf056afb33ea8a92b2508e57ea7
Daniel <daniel.castelline@gmail.com>
2021-03-12 14:20:49 -0600

    initial commit

---
+++ client/build/precache-manifest.855b59ce9a447ca85cd8cb7d6eca15fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6cd14dcab190e1de95fda32a5c65d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978d40a2d7c3042e122e",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "c8c0fc73da2130049b2e",
+    "url": "/static/js/2.1f693a0e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.1f693a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978d40a2d7c3042e122e",
+    "url": "/static/js/main.ecf67ea4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
